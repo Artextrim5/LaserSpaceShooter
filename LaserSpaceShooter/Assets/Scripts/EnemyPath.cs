58632981b9f6ffea7a940f3543edab7779c4b34c
Artextrim5@gmail.com <Artextrim5@gmail.com>
2021-06-29 23:39:04 +0300

    Сделал  спавн волны врагов

---
+++ LaserSpaceShooter/Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> wayPoints;
+    [SerializeField] float moveSpeed = 2f;
+    int wayPointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wayPoints = waveConfig.GetWayPoints();
+        transform.position = wayPoints[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMove();
+    }
+
+    private void EnemyMove()
+    {
+        if (wayPointIndex <= wayPoints.Count - 1)
+        {
+            var targetPosition = wayPoints[wayPointIndex].transform.position;
+            var movementThisFraim = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFraim);
+
+            if (transform.position == targetPosition)
+            {
+                wayPointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
